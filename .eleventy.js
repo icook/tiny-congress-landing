00c88534333be23cd79219d9a42e5003b54aa4c1
Isaac Cook <isaac@simpload.com>
2025-12-13 00:46:27 -0600

    Configure custom domain tinycongress.com

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 module.exports = function(eleventyConfig) {
   // Copy the css directory to the output
   eleventyConfig.addPassthroughCopy("src/css");
-  
+
+  // Copy CNAME file for custom domain
+  eleventyConfig.addPassthroughCopy("CNAME");
+
   return {
     dir: {
       input: "src",
